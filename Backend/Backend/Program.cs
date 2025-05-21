7c496b4601966ca8e60c4ac5fc00443e004da35b
Maximiliano Orellana <maxiorellana555@gmail.com>
2025-05-21 18:37:37 -0300

    fix: conectar a la DB

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -11,19 +11,18 @@ builder.Configuration
 var DB_Server = builder.Configuration.GetConnectionString("Server");
 var DB_Port = builder.Configuration.GetConnectionString("Port");
 var DB_Name = builder.Configuration.GetConnectionString("Database");
-var DB_UserName = Environment.GetEnvironmentVariable("DB_USER");
-var DB_UserPasswrod = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var DB_UserName = builder.Configuration.GetConnectionString("Username");
+var DB_UserPasswrod = builder.Configuration.GetConnectionString("Password");
 var DB_ConfigurationString = $"server={DB_Server};port={DB_Port};database={DB_Name};user={DB_UserName};password={DB_UserPasswrod}";
-
-
-var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
 
 builder.Services.AddDbContext<DbContext>(options =>
     options.UseMySql(
         DB_ConfigurationString,
         ServerVersion.AutoDetect(DB_ConfigurationString)
         ));
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
