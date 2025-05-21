b7a54eda5742b639346084a6af9e55bcc804f3a2
Maximiliano Orellana <maxiorellana555@gmail.com>
2025-05-21 20:50:29 -0300

    feat: Migrar tabla Facultad

---
+++ Backend/Backend/Models/Facultad.cs
@@ -0,0 +1,12 @@
+
+namespace VocacionPlus.Models
+{
+    public class Facultad
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public string Imagen { get; set; }
+        public string Url { get; set; }
+    }
+}
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using VocacionPlus.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false)
+    .AddJsonFile("appsettings.User.json", optional: false)
     .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
     .AddEnvironmentVariables();
 
+// cadena de conexion
 var DB_Server = builder.Configuration.GetConnectionString("Server");
 var DB_Port = builder.Configuration.GetConnectionString("Port");
 var DB_Name = builder.Configuration.GetConnectionString("Database");
@@ -15,14 +18,18 @@ var DB_UserName = builder.Configuration.GetConnectionString("Username");
 var DB_UserPasswrod = builder.Configuration.GetConnectionString("Password");
 var DB_ConfigurationString = $"server={DB_Server};port={DB_Port};database={DB_Name};user={DB_UserName};password={DB_UserPasswrod}";
 
-builder.Services.AddDbContext<DbContext>(options =>
-    options.UseMySql(
-        DB_ConfigurationString,
-        ServerVersion.AutoDetect(DB_ConfigurationString)
-        ));
+// agregar el contexto de la base de datos
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseMySql(DB_ConfigurationString, ServerVersion.AutoDetect(DB_ConfigurationString))
+    // las proximas 3 lineas son para el desarrollo y debugging,
+    // deberian ser eliminadas o cambiadas para produccion
+    .LogTo(Console.WriteLine, LogLevel.Information)
+    .EnableSensitiveDataLogging()
+    .EnableDetailedErrors()
+);
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => $"conectando a {DB_Name} en {DB_Server}:{DB_Port} con el usuario {DB_UserName}");
 
 app.Run();
