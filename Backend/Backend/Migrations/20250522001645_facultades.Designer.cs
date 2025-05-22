476297fa5e858d0eee1879943d9d7f5a7e6f5103
Maximiliano Orellana <maxiorellana555@gmail.com>
2025-05-21 21:19:12 -0300

    feat: hacer idFacultad autoincremental

--- Backend/Backend/Migrations/20250522001645_facultades.Designer.cs
+++ Backend/Backend/Migrations/20250522001645_facultades.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VocacionPlus.Database;
 
@@ -10,9 +11,11 @@ using VocacionPlus.Database;
 namespace Backend.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250522001645_facultades")]
+    partial class facultades
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Backend/Backend/Models/Facultad.cs
+++ Backend/Backend/Models/Facultad.cs
@@ -1,8 +1,15 @@
 
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace VocacionPlus.Models
 {
     public class Facultad
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        [Required]
         public int Id { get; set; }
         public string Nombre { get; set; }
         public string Descripcion { get; set; }
