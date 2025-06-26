e931d72f64012fb86871352fc1a3221b4d120e5d
Maurinho <Mauricitoxx>
2025-06-26 16:53:53 -0300

    router

--- Frontend/vocacion_plus/src/App.js
+++ Frontend/vocacion_plus/src/App.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container } from '@mui/material';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import VerFacultades from './Components/VerFacultad/VerFacultad';
 import ListadoDeFacultades from './Components/VerFacultad/ListadoDeFacultades';
+import FacultadCard from './Components/VerFacultad/FacultadCard';
 
 const App = () => {
-  const [selectedFacultadId, setSelectedFacultadId] = useState(null);
-
-  const handleViewFacultad = (id) => {
-    setSelectedFacultadId(id);
-  };
-
-  const handleGoBack = () => {
-    setSelectedFacultadId(null);
-  };
-
   return (
-    <Container maxWidth="lg" >
-
-    </Container>
+    <Router>
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <Routes>
+          <Route path="/" element={<ListadoDeFacultades />} />
+          <Route path="/facultad/:id" element={<VerFacultades />} />
+          <Route path="/facultad/:id/card" element={<FacultadCard />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 };
 
