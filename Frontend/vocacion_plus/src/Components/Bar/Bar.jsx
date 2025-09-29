648dcbc8220ca5d3e321e0148d9efe778457ed41
MaximilianoOcatavioOrellana <maxiorellana555@gmail.com>
2025-09-29 15:23:34 -0300

    refactor: incluir MUI
    Inclui la libreria MUI al proyecto y le agrege una variable para las
    rutas.

--- Frontend/vocacion_plus/src/Components/Bar/Bar.jsx
+++ Frontend/vocacion_plus/src/Components/Bar/Bar.jsx
@@ -1,36 +1,108 @@
 import React from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Bar.css";
-import menu from "../images/menu.png";
-import user from "../images/user.png";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { Drawer, List, ListItem, ListItemButton, ListItemText } from "@mui/material";
 
-const Bar = () => {
+export default function Bar() {
+  // aca pongan las opciones del menu (tienen que estar con el link en el app.js)
+  const routes = [
+    { text: "Facultades", path: "/facultad" },
+    { text: "Carreras", path: "/carrera" },
+    { text: "Usuario", path: "/usuario" },
+    { text: "Login", path: "/login" },
+    { text: "Registro", path: "/register" },
+  ];
+
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const [open, setOpen] = useState(false);
+  const toggleDrawer = (newOpen) => () => {
+    setOpen(newOpen);
+  };
+
   return (
-// Bar.js (fragmento relevante)
-<div className="bar">
-  <div className="menu">
-    <img className="menu1" src={menu} alt="Menu" />
-
-    {/* Sidebar oculto que se abre al hover sobre la imagen */}
-    <div className="sidebar">
-      <ul>
-        <li><a href="/facultad">Facultad</a></li>
-        <li><a href="/carreras">Carreras</a></li>
-        <li><a href="/usuario">Usuario</a></li>
-        <li><a href="/login">Login</a></li>
-        <li><a href="/register">Register</a></li>
-      </ul>
-    </div>
-  </div>
-
-  <h1 className="nombre">Vocación+</h1>
-
-  <div className="user">
-    <img className="user1" src={user} alt="user" />
-  </div>
-</div>
-
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleDrawer(true)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer open={open} onClose={toggleDrawer(false)}>
+            <List>
+              {routes.map((item, index) => (
+              <ListItem key={index}>
+                <ListItemButton component={Link} to={item.path}>
+                  <ListItemText primary={item.text} />
+                </ListItemButton>
+              </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: "center" }}>
+            Vocación Plus
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
-};
-
-export default Bar;
+}
