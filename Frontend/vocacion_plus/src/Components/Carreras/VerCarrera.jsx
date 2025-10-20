cb47b72bee1dbdde7eec863c222277431e5e888d
MaximilianoOcatavioOrellana <maxiorellana555@gmail.com>
2025-10-20 13:22:30 -0300

    ultimo cambio

--- Frontend/vocacion_plus/src/Components/Carreras/VerCarrera.jsx
+++ Frontend/vocacion_plus/src/Components/Carreras/VerCarrera.jsx
@@ -1,11 +1,39 @@
+import { Breadcrumbs, Divider, Link, Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Link as RouterLink, Router, useParams } from "react-router-dom";
 
 const VerCarrera = () => {
   const parametro = useParams();
   const carrera = parametro.nombre; // con esto busco la carrera
   // console.log(carrera)
-  return (<></>);
+  return (
+    <Box>
+      <Breadcrumbs sx={{
+        color: 'var(--primary-100)',
+        fontWeight: 'bold',
+        fontSize: '2.5rem',
+      }}>
+        <Link
+          component={RouterLink}
+          underline="hover"
+          color='var(--primary-100)'
+          to="/carrera"
+        > Carreras
+        </Link>
+        <Typography sx={{
+          fontSize: '1.5rem',
+        }}>
+          {carrera}
+        </Typography>
+      </Breadcrumbs>
+      <Divider variant="middle" sx={{
+        bgcolor: 'white',
+        width: '90vw',
+        mx: 'auto'
+      }} />
+    </Box>
+  );
 }
 
 export default VerCarrera;
