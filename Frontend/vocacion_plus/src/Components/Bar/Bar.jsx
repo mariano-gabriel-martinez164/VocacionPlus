2c829797118052d72a1ecbd4275c1b3fa8a4d2bb
MaximilianoOcatavioOrellana <maxiorellana555@gmail.com>
2025-10-22 00:43:45 -0300

    fix: conflictos con el merge

--- Frontend/vocacion_plus/src/Components/Bar/Bar.jsx
+++ Frontend/vocacion_plus/src/Components/Bar/Bar.jsx
@@ -32,10 +32,6 @@ export default function Bar() {
     setOpen(newOpen);
   };
 
-<<<<<<< HEAD
-
-
-
   useEffect(() => {
     if(token) {
       try {
@@ -63,37 +59,6 @@ export default function Bar() {
         { text: "Buscar Carreras", path: "/carrera" },
     ];
 
-
-=======
-  useEffect(() => {
-    if(token) {
-      try {
-        const decodedToken = jwtDecode(token);
-        setLogin(true);
-        setRol(decodedToken.role)
-      }catch(error) {
-        console.error("token vencido:", error);
-        localStorage.removeItem("token");
-        setLogin(false);
-      }
-    } else {
-      setLogin(false);
-    }
-  }, [token]);
-   
-   const routes = rol === "Admin" 
-   ? [
-      { text: "Gestionar Facultades", path: "/" },
-      { text: "Gestionar Carreras", path: "/carrera" },
-      { text: "Gestionar Usuarios", path: "/usuarios" },
-    ]
-    : [
-        { text: "Buscar Facultades", path: "/" },
-        { text: "Buscar Carreras", path: "/carrera" },
-    ];
-
-
->>>>>>> 47a3dcd5efa66da4e55b80c6dc3bdacddae8169d
   return (
     <Box sx={{ flexGrow: 1 }} className="box">
       <AppBar position="static" className="bar">
