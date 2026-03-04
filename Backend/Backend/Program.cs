0eb93f2ed314c6b085b6d9f0ed98ca0a12c96a12
mariano <martinez25mariano25@gmail.com>
2026-03-04 01:18:44 -0300

    fix: cors

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
     policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:5173")
         .AllowAnyHeader()
         .AllowAnyMethod();
     });
